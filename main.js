d37159fc233586e0294ccf0324580202a0931ac2
TheCanonKanon <robert_wollny@web.de>
2024-06-21 16:23:32 +0200

    initial

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+//getting all public repos from user
+//we can probably sort them by some metrics
+https://api.github.com/users/TheCanonKanon/repos
+
+//find the repos and look thru all of em to find the weekdays on which the most commits happen?
+//Is that a bit much requests?
+https://api.github.com/users/TheCanonKanon/repos
+https://api.github.com/repos/TheCanonKanon/CodeAcademyBerlin_Module1_Project1/commits
