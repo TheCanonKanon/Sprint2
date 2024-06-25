22d3f37bfc0f68c69e28e6a58ae1418e49cdd3b7
TheCanonKanon <robert.wollny3@gmail.com>
2024-06-25 12:15:07 +0200

    counts the days on which the most commits happend

--- main.js
+++ main.js
@@ -14,27 +14,39 @@
 //that might take a while to process
 //https://api.github.com/repositories
 
-function getInformations (reposObject) {
+
+//find the top 3 "best" public repos of user
+async function top3Repos (repos) {
     
 }
 
-function repoCommitsFetch (urlCommits) {
-    fetch (urlCommits)
-        .then (resp => resp.json())
-        .then (result => {return(result);})
+
+//get the productive days from all commits a user did
+async function repoCommitsFetch (repos, userName) {
+    //sunday to saturday
+    const productiveDates = [0,0,0,0,0,0,0]
+    for (let x of repos) {
+        const commits = await fetch (x.commits_url.replace("{/sha}",""));
+        const commitsJSON = await commits.json();
+        for (let y of commitsJSON) {
+            if (y.commit.author.name === userName) {
+                const date = new Date(y.commit.author.date);
+                productiveDates[date.getDay()]++;
+            }
+        }
+    }
+    console.log(...productiveDates);
 }
 
 async function userRepoFetch () {
     let userName = document.querySelector("#username-search").value;
+    console.log("test4");
     const response = await fetch (`https://api.github.com/users/${userName}/repos`)
-    console.log("test");
+    console.log("test3");
     const responseJSON = await response.json()
-    console.log(responseJSON)
-    return responseJSON;
+    repoCommitsFetch (responseJSON,userName)
     }
 
-    console.log("test");
-console.log(userRepoFetch());
-console.log("test2");
+//console.log(userRepoFetch());
 
 
